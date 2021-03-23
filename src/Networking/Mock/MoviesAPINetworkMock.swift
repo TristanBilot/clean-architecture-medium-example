04e0a679ce79ab15e36ffa3bbc4acc0dccc87f2b
Tristan BILOT <tristan_bilot@carrefour.com>
2021-03-23 11:43:46 +0100

    feat(movies): replace network mock by real Movie

--- src/Networking/Mock/MoviesAPINetworkMock.swift
+++ src/Networking/Mock/MoviesAPINetworkMock.swift
@@ -8,6 +8,7 @@
 /* This class is a mock used as an example, it should be replaced with real API callsx */
 class MoviesAPINetworkMock: APINetwork {
   func execute<T>(request: APIRequest, completion: @escaping (Result<T, Error>) -> Void) {
-    completion(.success("yes" as! T))
+    let mock = Movie(category: .horror, name: "Evil dead", date: 1983)
+    completion(.success(mock as! T))
   }
 }
